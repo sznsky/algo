8d78dcafd7a78511beb06b9022c067d7bfebd26e
sznsky <sznsky@qq.com>
2022-05-31 21:00:19 +0800

    简单计算器

---
+++ algo-jike/src/main/java/preview/BasicCalculatorII.java
@@ -0,0 +1,22 @@
+package preview;
+
+/**
+ * 输入：s = "3+2*2"
+ * 输出：7
+ * 输入：s = " 3/2 "
+ * 输出：1
+ * leetcode:227
+ *  基本计算器 II
+ * 简单计算器实现
+ */
+public class BasicCalculatorII {
+
+    //"3+2*2" 3 2  
+
+
+
+
+
+    
+
+}
