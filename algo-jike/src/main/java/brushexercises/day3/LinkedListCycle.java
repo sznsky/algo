301cced6dfc0f24a71845902afd7dc9e5040f39e
sznsky <sznsky@qq.com>
2022-05-31 19:22:30 +0800

    每日一题

---
+++ algo-jike/src/main/java/brushexercises/day3/LinkedListCycle.java
@@ -0,0 +1,58 @@
+package brushexercises.day3;
+
+import comm.ListNode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Describe :
+ * @Author : sunzhenning
+ * @Since : 2022/5/31 18:40
+ */
+public class LinkedListCycle {
+
+    /**
+     * 暴力解法：
+     * 1.遍历链表
+     * 2.将每个链表头放到集合中
+     * 3.如果集合中存在这个链表，说明是存在环形链表
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while(head != null){
+            list.add(head);
+            head = head.next;
+            if(list.contains(head)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 优化解法
+     * 使用set存放链表，能够有效的节约内存
+     * @param head
+     * @return
+     */
+    public boolean hasCycle1(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while(head != null){
+            if(!set.add(head)){
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+
+
+
+
+}
