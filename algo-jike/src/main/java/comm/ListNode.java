0911f9b57b76824dfc4cddf6fa3a716f91a50ba7
sznsky <sznsky@qq.com>
2022-05-31 18:18:44 +0800

    每日一题

---
+++ algo-jike/src/main/java/comm/ListNode.java
@@ -0,0 +1,14 @@
+package comm;
+
+/**
+ * @Describe : 链表类
+ * @Author : sunzhenning
+ * @Since : 2022/5/31 14:26
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
