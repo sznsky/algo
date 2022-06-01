11243f409e01efc0097b1fed242061ca2911e7a4
sznsky <sznsky@qq.com>
2022-06-01 20:48:22 +0800

    提交

---
+++ algo-jike/src/main/java/week1/classroom/ReverseNodesInKGroup.java
@@ -0,0 +1,17 @@
+package week1.classroom;
+
+import comm.ListNode;
+
+/**
+ * @Describe :
+ * @Author : sunzhenning
+ * @Since : 2022/6/1 20:47
+ */
+public class ReverseNodesInKGroup {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        return null;
+    }
+
+}
