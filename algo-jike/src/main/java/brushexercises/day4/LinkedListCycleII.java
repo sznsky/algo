02fb185a31f1921b3023ca8aaa1c2e48d809b952
sznsky <sznsky@qq.com>
2022-06-01 20:51:36 +0800

    添加注释

--- algo-jike/src/main/java/brushexercises/day4/LinkedListCycleII.java
+++ algo-jike/src/main/java/brushexercises/day4/LinkedListCycleII.java
@@ -14,6 +14,11 @@ import java.util.Set;
  */
 public class LinkedListCycleII {
 
+    /**
+     * 实现方式
+     * @param head
+     * @return
+     */
     public ListNode detectCycle(ListNode head) {
         Map<ListNode,Integer> map = new HashMap<>();
         int i = 0;
@@ -28,6 +33,11 @@ public class LinkedListCycleII {
         return null;
     }
 
+    /**
+     * 实现方式1
+     * @param head
+     * @return
+     */
     public ListNode detectCycle1(ListNode head) {
         Set<ListNode> set = new HashSet<>();
         while(head != null){
