7b09068a7e065e30276166e3e8f7cdce5f604d35
sznsky <sznsky@qq.com>
2022-06-06 11:49:06 +0800

    滑动窗口最大值

--- algo-jike/src/main/java/week1/classroom/SlidingWindowMaximum.java
+++ algo-jike/src/main/java/week1/classroom/SlidingWindowMaximum.java
@@ -9,18 +9,22 @@ import java.util.LinkedList;
  */
 public class SlidingWindowMaximum {
 
+
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> deque = new LinkedList<>();
         int[] answer = new int[nums.length-k+1];
         for(int i=0;i<nums.length;i++){
+            while(!deque.isEmpty() && deque.getFirst() <= i-k){
+                deque.removeFirst();
+            }
             while(!deque.isEmpty() && nums[deque.getLast()] < nums[i]){
                 deque.removeLast();
             }
             deque.push(i);
+            if(i >= k-1){
+                answer[i-(k-1)] = nums[deque.getFirst()];
+            }
         }
-
-
-
-        return null;
+        return answer;
     }
 }
