b10a68525e7f7be89335774c3ae975340ca630a6
sznsky <sznsky@qq.com>
2022-06-06 18:07:00 +0800

    滑动窗口-单调队列思想

--- algo-jike/src/main/java/week1/classroom/SlidingWindowMaximum.java
+++ algo-jike/src/main/java/week1/classroom/SlidingWindowMaximum.java
@@ -1,13 +1,24 @@
 package week1.classroom;
 
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
 /**
  * leetcode : 239 【滑动窗口最大值】
  * 考察知识点：单调队列思想
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
  */
 public class SlidingWindowMaximum {
+
+    public static void main(String[] args) {
+        SlidingWindowMaximum slidingWindowMaximum = new SlidingWindowMaximum();
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        int[] result = slidingWindowMaximum.maxSlidingWindow(nums, k);
+        System.out.println(Arrays.toString(result));
+    }
 
 
     public int[] maxSlidingWindow(int[] nums, int k) {
@@ -20,7 +31,7 @@ public class SlidingWindowMaximum {
             while(!deque.isEmpty() && nums[deque.getLast()] < nums[i]){
                 deque.removeLast();
             }
-            deque.push(i);
+            deque.addLast(i);
             if(i >= k-1){
                 answer[i-(k-1)] = nums[deque.getFirst()];
             }
