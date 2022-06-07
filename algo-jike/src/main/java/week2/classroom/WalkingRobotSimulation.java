5b21b8e71be145f9624140be4f476c99c775a9f3
sznsky <sznsky@qq.com>
2022-06-07 10:24:00 +0800

    1机器人走路2域名访问计数器

---
+++ algo-jike/src/main/java/week2/classroom/WalkingRobotSimulation.java
@@ -0,0 +1,14 @@
+package week2.classroom;
+
+/**
+ * @Describe : leetcode:874 【模拟行走的机器人】
+ * @Author : sunzhenning
+ * @Since : 2022/6/7 10:20
+ */
+public class WalkingRobotSimulation {
+
+    public int robotSim(int[] commands, int[][] obstacles) {
+
+        return 0;
+    }
+}
---
+++ algo-jike/src/main/java/week2/task/SubdomainVisitCount.java
@@ -0,0 +1,12 @@
+package week2.task;
+
+/**
+ * @Describe : leetcode:811 【子域名访问计数】
+ * @Author : sunzhenning
+ * @Since : 2022/6/7 10:22
+ */
+public class SubdomainVisitCount {
+
+
+
+}
