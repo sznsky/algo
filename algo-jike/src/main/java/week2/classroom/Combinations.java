63c792412cf47e96cff4cf59c5cd353e12b672b4
sznsky <sznsky@qq.com>
2022-06-08 09:14:56 +0800

    组合

---
+++ algo-jike/src/main/java/week2/classroom/Combinations.java
@@ -0,0 +1,40 @@
+package week2.classroom;
+
+import java.util.List;
+
+/**
+ * @Describe : 组合 leetcode:77
+ * @Author : sunzhenning
+ * @Since : 2022/6/7 18:09
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * 你可以按 任何顺序 返回答案。
+ * 思路：递归算法
+ */
+public class Combinations {
+
+    private int n;
+    private int k;
+    private List<List<Integer>> ans;
+    private List<Integer> s;
+
+    public List<List<Integer>> combine(int n, int k) {
+
+
+        return null;
+    }
+
+    private void recur(int index){
+
+        if(index == n+1){
+
+        }
+
+    }
+
+
+
+
+
+
+
+}
