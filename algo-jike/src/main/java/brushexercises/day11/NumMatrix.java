cb7d2a664793db9c5f60fc875ed5722c1e40fdb3
sznsky <sznsky@qq.com>
2022-06-08 22:26:22 +0800

    添加注释

--- algo-jike/src/main/java/brushexercises/day11/NumMatrix.java
+++ algo-jike/src/main/java/brushexercises/day11/NumMatrix.java
@@ -1,7 +1,5 @@
 package brushexercises.day11;
 
-import java.util.Arrays;
-
 /**
  * @Describe : 二维数组求和 leetcode:304
  * @Author : sunzhenning
@@ -23,6 +21,7 @@ public class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
         if(matrix.length >0){
+            //前置二维数组row和col加1的原因，防止下面的i-1或者j-1超出边界
             preSums = new int[matrix.length+1][matrix[0].length+1];
             for(int i=0;i<matrix.length;i++){
                 for(int j=0;j<matrix[0].length;j++){
@@ -33,6 +32,8 @@ public class NumMatrix {
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
+        //原推到的方法为：preSums[row2][col2] - preSums[row2][col1-1] - preSums[row1-1][col2] + preSums[row1-1][col1-1];
+        //下面的下标都是1的情况下，因为preSums这个二维数组的row和col都扩大了一个1
         return preSums[row2+1][col2+1] - preSums[row2+1][col1] - preSums[row1][col2+1] + preSums[row1][col1];
     }
 }
