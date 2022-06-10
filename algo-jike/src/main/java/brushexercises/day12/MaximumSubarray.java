6680f92b687f1ea3b159f4979b878d30960ffa18
sznsky <sznsky@qq.com>
2022-06-10 13:41:48 +0800

    最大子数组的和

--- algo-jike/src/main/java/brushexercises/day12/MaximumSubarray.java
+++ algo-jike/src/main/java/brushexercises/day12/MaximumSubarray.java
@@ -8,9 +8,15 @@ package brushexercises.day12;
 public class MaximumSubarray {
 
     public int maxSubArray(int[] nums) {
-
-
-
-        return 0;
+        int ans = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]+nums[i-1] > nums[i]){
+                nums[i] = nums[i]+nums[i-1];
+            }
+            if(nums[i] > ans){
+                ans = nums[i];
+            }
+        }
+        return ans;
     }
 }
