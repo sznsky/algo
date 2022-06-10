718b01a85fab122b49d8fceee876f658f5ae50c5
sznsky <sznsky@qq.com>
2022-06-10 18:35:03 +0800

    三数之和

---
+++ algo-jike/src/main/java/brushexercises/day13/ThreeNumSum.java
@@ -0,0 +1,77 @@
+package brushexercises.day13;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+* @Describe :
+* @Author : sunzhenning
+* @Since : 2022/6/10 15:38
+ * 思路:使用递归，找出所有三个数组合数组，从中找出等于0的数组
+*/
+public class ThreeNumSum {
+
+    public static void main(String[] args) {
+        ThreeNumSum threeNumSum = new ThreeNumSum();
+        int[] nums = {3,-3,0};
+        threeNumSum.threeSum(nums);
+        System.out.println(Arrays.toString(threeNumSum.ans.toArray()));
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        ans = new ArrayList<>();
+        s = new ArrayList<>();
+        set = new HashSet();
+        combinations(0, 3, nums);
+        return ans;
+    }
+
+    private List<List<Integer>> ans;
+    private List<Integer> s;
+    private Set<String> set;
+
+    public void combinations(int index, int k,int[] nums) {
+        if (index >= nums.length || s.size() > 3) {
+            return;
+        }
+        if (s.size() == k) {
+            int sum = 0;
+            StringBuilder stringBuilder = new StringBuilder();
+            List<Integer> tempS = s;
+            //将s排序,拼接，存入set用作判断是否存在（三个元素不分顺序）
+            tempS = tempS.stream().sorted().collect(Collectors.toList());
+            for (int i = 0; i < k; i++) {
+                sum += tempS.get(i);
+                stringBuilder.append(tempS.get(i));
+            }
+            if(sum == 0 && !set.contains(stringBuilder.toString())){
+                ans.add(new ArrayList<>(tempS));
+                set.add(stringBuilder.toString());
+            }
+            return;
+        }
+        combinations(index + 1, k, nums);
+        s.add(nums[index]);
+        combinations(index + 1, k, nums);
+        s.remove(s.size() - 1);
+    }
+
+    /**
+     *
+     * @param nums
+     * @return
+     */
+    private int sum(int[] nums){
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+
+
+
+
+
+}
