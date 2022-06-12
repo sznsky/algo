417bcc163d9ccf3066426d9ba5fc306a8fd2961e
sunzhenning <sunzhenning@khtong.cn>
2022-06-12 11:31:07 +0800

    叉树的前序遍历

---
+++ algo-jike/src/main/java/week3/classroom/NAryTreePreorderTraversal.java
@@ -0,0 +1,40 @@
+package week3.classroom;
+
+import comm.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * leetcode:589. N 叉树的前序遍历
+ * 思路：递归方法
+ * 前序遍历：根->左->右
+ */
+public class NAryTreePreorderTraversal {
+
+    /**
+     * 存放结果
+     */
+    private List<Integer> seq;
+
+    public List<Integer> preorder(Node root) {
+        seq = new ArrayList<>();
+        dfs(root);
+        return seq;
+    }
+
+    /**
+     * 递归遍历：根->左->右
+     * @param root
+     */
+    private void dfs(Node root){
+        if(root == null){
+            return;
+        }
+        seq.add(root.val);
+        List<Node> children = root.children;
+        for(Node node : children){
+            dfs(node);
+        }
+    }
+}
