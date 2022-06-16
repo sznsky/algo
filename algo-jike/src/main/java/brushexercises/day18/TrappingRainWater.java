9b58858479159c6af4ac20cfa36c17255cbf458b
sznsky <sznsky@qq.com>
2022-06-16 18:18:51 +0800

    接雨水，单调栈

---
+++ algo-jike/src/main/java/brushexercises/day18/TrappingRainWater.java
@@ -0,0 +1,15 @@
+package brushexercises.day18;
+
+/**
+ * @Describe :  leetcode:42. 接雨水
+ * @Author : sunzhenning
+ * @Since : 2022/6/16 18:08
+ */
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+
+        return 0;
+    }
+
+}
