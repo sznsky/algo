0558ac5f4bc260e4a5d4c14576dca8ea7e4bd4c6
sznsky <sznsky@qq.com>
2022-06-23 22:33:36 +0800

    leetcode:875. 爱吃香蕉的珂珂

---
+++ algo-jike/src/main/java/week4/task/KokoEatingBananas.java
@@ -0,0 +1,28 @@
+package week4.task;
+
+/**
+ * @Describe : leetcode:875. 爱吃香蕉的珂珂
+ * @Author : sunzhenning
+ * @Since : 2022/6/23 19:56
+ * 思路：二分
+ */
+public class KokoEatingBananas {
+
+    /**
+     * 思路分析：珂珂爱吃香蕉，每个小时最大吃香蕉量为所有的香蕉之和，最小吃香蕉量
+     * @param piles
+     * @param h
+     * @return
+     */
+    public int minEatingSpeed(int[] piles, int h) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
