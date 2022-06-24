78bceb2d63bf3c86967601dc253df2eb65695381
sznsky <sznsky@qq.com>
2022-06-25 03:46:31 +0800

    leetcode:450. 删除二叉搜索树中的节点

---
+++ algo-jike/src/main/java/brushexercises/day28/DeleteNodeInABst.java
@@ -0,0 +1,16 @@
+package brushexercises.day28;
+
+import comm.TreeNode;
+
+/**
+ * @Describe : leetcode:450. 删除二叉搜索树中的节点
+ * @Author : sunzhenning
+ * @Since : 2022/6/25 3:45
+ */
+public class DeleteNodeInABst {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return null;
+    }
+
+}
