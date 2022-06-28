214dc02ed3741ed910f3999cbd0ed28acdb21e80
sznsky <sznsky@qq.com>
2022-06-28 15:12:49 +0800

    leetcode:142. 环形链表 II

--- algo-jike/src/main/java/brushexercises/day30/NO_142_LinkedListCycleII.java
+++ algo-jike/src/main/java/brushexercises/day30/NO_142_LinkedListCycleII.java
@@ -3,7 +3,7 @@ package brushexercises.day30;
 import comm.ListNode;
 
 /**
- * @Describe :
+ * @Describe : leetcode:142. 环形链表 II
  * @Author : sunzhenning
  * @Since : 2022/6/28 10:28
  */
@@ -15,11 +15,16 @@ public class NO_142_LinkedListCycleII {
         while (fast != null && fast.next !=null){
             fast = fast.next.next;
             slow = slow.next;
+            //快慢指针相遇了
             if(fast == slow){
-
+                //移动慢指针和头部指针，他们相遇的地方就是环开始的地方
+                while(slow != head){
+                    slow = slow.next;
+                    head = head.next;
+                }
+                return head;
             }
         }
-
         return null;
     }
 }
