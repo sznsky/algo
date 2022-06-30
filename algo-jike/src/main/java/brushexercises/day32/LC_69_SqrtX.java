1aa45abaea270ea92663b103d7bdc63d73480a48
sznsky <sznsky@qq.com>
2022-06-30 11:20:07 +0800

    69. x 的平方根

---
+++ algo-jike/src/main/java/brushexercises/day32/LC_69_SqrtX.java
@@ -0,0 +1,14 @@
+package brushexercises.day32;
+
+/**
+ * @Describe : 69. x 的平方根
+ * @Author : sunzhenning
+ * @Since : 2022/6/30 11:18
+ */
+public class LC_69_SqrtX {
+
+    public int mySqrt(int x) {
+
+        return 0;
+    }
+}
