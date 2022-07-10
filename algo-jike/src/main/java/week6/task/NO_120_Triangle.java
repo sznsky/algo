c4293ea6e57ab87d1dc4efde97cc4cfd0426a723
sunzhenning <sunzhenning@khtong.cn>
2022-07-10 22:39:11 +0800

    leetcode:120. 三角形最小路径和

--- algo-jike/src/main/java/week6/task/NO_120_Triangle.java
+++ algo-jike/src/main/java/week6/task/NO_120_Triangle.java
@@ -7,10 +7,33 @@ import java.util.List;
  */
 public class NO_120_Triangle {
 
+    /**
+     * 思路：递归+缓存（记忆缓存）
+     * @param triangle
+     * @return
+     */
     public int minimumTotal(List<List<Integer>> triangle) {
+        int[][] dp = new int[triangle.size()][triangle.size()];
+        return  dfs(triangle, 0, 0,dp);
+    }
 
-        return 0;
-
+    /**
+     * 深度遍历
+     * @param triangle
+     * @param i
+     * @param j
+     * @param dp
+     * @return
+     */
+    private int dfs(List<List<Integer>> triangle, int i, int j,int[][] dp) {
+        if (i == triangle.size()) {
+            return 0;
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        return dp[i][j] = Math.min(dfs(triangle, i + 1, j,dp),
+                dfs(triangle, i + 1, j + 1,dp)) + triangle.get(i).get(j);
     }
 
 }
