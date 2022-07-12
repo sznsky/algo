a83e4b696548c9a0198a8a0891c9740d74f0bd5e
sznsky <sznsky@qq.com>
2022-07-12 11:17:57 +0800

    提交

---
+++ algo-jike/src/main/java/brushexercises/day43/NO_300_LongestIncreasingSubsequence.java
@@ -0,0 +1,16 @@
+package brushexercises.day43;
+
+/**
+ * @Describe :
+ * @Author : sunzhenning
+ * @Since : 2022/7/12 11:17
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4
+ */
+public class NO_300_LongestIncreasingSubsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        return 0;
+    }
+}
