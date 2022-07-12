992e16a3813806243686cbea5b95318a0ac3793a
sznsky <sznsky@qq.com>
2022-07-12 15:28:12 +0800

    152. 乘积最大子数组

---
+++ algo-jike/src/main/java/brushexercises/day44/NO_152_MaximumProductSubarray.java
@@ -0,0 +1,17 @@
+package brushexercises.day44;
+
+/**
+ * @Describe :152. 乘积最大子数组
+ * @Author : sunzhenning
+ * @Since : 2022/7/12 15:26
+ * 给你一个整数数组 nums，请你找出数组中乘积最大的非空连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ * 测试用例的答案是一个32-位 整数。
+ * 子数组 是数组的连续子序列。
+ */
+public class NO_152_MaximumProductSubarray {
+
+    public int maxProduct(int[] nums) {
+
+        return 0;
+    }
+}
