4a591806c9d9b824f8033fe6b601100e166aa634
sznsky <sznsky@qq.com>
2022-07-12 21:47:35 +0800

    206. 反转链表

---
+++ algo-jike/src/main/java/brushexercises/day2/NO_206_ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package brushexercises.day2;
+
+import comm.ListNode;
+
+/**
+ * 206. 反转链表
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * @Describe :
+ * @Author : sunzhenning
+ * @Since : 2022/5/31 14:23
+ */
+public class NO_206_ReverseLinkedList {
+
+    public ListNode reverse(ListNode head){
+        ListNode last = null;
+        while(head != null){
+            ListNode nextHead = head.next;
+            head.next = last;
+            last = head;
+            head = nextHead;
+        }
+        return last;
+    }
+
+}
--- algo-jike/src/main/java/brushexercises/day2/NO_206_ReverseLinkedList1.java
+++ algo-jike/src/main/java/brushexercises/day2/NO_206_ReverseLinkedList1.java
@@ -3,17 +3,18 @@ package brushexercises.day2;
 import comm.ListNode;
 
 /**
+ * 206. 反转链表
  * 输入：head = [1,2,3,4,5]
  * 输出：[5,4,3,2,1]
  * @Describe :
  * @Author : sunzhenning
  * @Since : 2022/5/31 14:23
  */
-public class ReverseLinkedList {
+public class NO_206_ReverseLinkedList1 {
 
     public ListNode reverse(ListNode head){
         ListNode last = null;
-        while(head != null){
+        while (head != null){
             ListNode nextHead = head.next;
             head.next = last;
             last = head;
