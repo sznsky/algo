cec8251f128ad8cf9ef0a81128412ed59f8d8ebc
sunzhenning <sunzhenning@khtong.cn>
2022-07-17 17:58:57 +0800

    55. 跳跃游戏

---
+++ algo-jike/src/main/java/week7/task/NO_45_JumpGameII.java
@@ -0,0 +1,13 @@
+package week7.task;
+
+/**
+ * 45. 跳跃游戏 II
+ */
+public class NO_45_JumpGameII {
+    public int jump(int[] nums) {
+
+
+        return 0;
+
+    }
+}
---
+++ algo-jike/src/main/java/week7/task/NO_516_LongestPalindromicSubsequence.java
@@ -0,0 +1,13 @@
+package week7.task;
+
+/**
+ * 516. 最长回文子序列
+ */
+public class NO_516_LongestPalindromicSubsequence {
+
+    public int longestPalindromeSubseq(String s) {
+
+
+        return 0;
+    }
+}
