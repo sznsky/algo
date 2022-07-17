82602a349150d15d21da2ac89deecfa571754a73
sunzhenning <sunzhenning@khtong.cn>
2022-07-17 15:06:26 +0800

    7周学习

---
+++ algo-jike/src/main/java/week7/example/NO_121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+package week7.example;
+
+/**
+ * 121. 买卖股票的最佳时机
+ */
+public class NO_121_BestTimeToBuyAndSellStock {
+
+
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        for(int i=0;i<prices.length;i++){
+
+        }
+        return 0;
+    }
+}
