e58e2b0e7c0ac5b237c6fefb73a869e4de903533
sznsky <sznsky@qq.com>
2022-07-21 21:55:12 +0800

    快速排序

--- algo-myself/src/main/java/sort/QuickSort.java
+++ algo-myself/src/main/java/sort/QuickSort.java
@@ -11,12 +11,16 @@ public class QuickSort {
 
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0,1,6,9,2,5,3,7,2,4,8,-1};
+        int[] nums = new int[]{3,1,6,9,2,5,3,7,2,4,8};
         quickSort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
     }
 
+    private static int wholeCount = 0;
+
     public static void quickSort(int[] arr,int start,int end){
+        wholeCount++;
+        System.out.println("================第"+wholeCount+"轮====================");
         if(start < end){
             //选择基准，也可以随机选择
             int base = arr[start];
@@ -32,9 +36,10 @@ public class QuickSort {
                 }
                 arr[high] = arr[low];
             }
-            //
+
+            //比较一轮了，现在重新选择基准。现在low=high了，arr[low]和arr[high]相等了，都可以赋给base
             arr[low] = base;
-            arr[high] = base;
+            //arr[high] = base;
             quickSort(arr, start,low);
             quickSort(arr, low+1,end);
         }
