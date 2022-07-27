44da402b6459337b28eac50e1df2cbdc776faede
sunzhenning <sunzhenning@khtong.cn>
2022-07-27 11:56:28 +0800

    28. 实现 strStr()

--- algo-jike/src/main/java/brushexercises/day58/NO_8_StringToIntegerAtoi.java
+++ algo-jike/src/main/java/brushexercises/day58/NO_8_StringToIntegerAtoi.java
@@ -1,12 +1,76 @@
 package brushexercises.day58;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Day58：字符串转换整数 (atoi)-8
  */
 public class NO_8_StringToIntegerAtoi {
 
+    /**
+     * 思路：状态机
+     * @param s
+     * @return
+     */
     public int myAtoi(String s) {
+        Automaton automaton = new Automaton();
+        int length = s.length();
+        for(int i=0;i<length;i++){
+            automaton.get(s.charAt(i));
+        }
+        return (int)(automaton.sign * automaton.ans);
+    }
 
-        return 0;
+    /**
+     * 自动机：有限状态机
+     */
+    class Automaton{
+        public int sign = 1;
+        public long ans = 0;
+        private String state = "start";
+        private Map<String, String[]> table = new HashMap<String, String[]>() {{
+            put("start", new String[]{"start", "signed", "in_number", "end"});
+            put("signed", new String[]{"end", "end", "in_number", "end"});
+            put("in_number", new String[]{"end", "end", "in_number", "end"});
+            put("end", new String[]{"end", "end", "end", "end"});
+        }};
+
+        /**
+         * 根据字符获取
+         * @param c
+         */
+        public void get(char c){
+            //根据字符获取状态
+            state = table.get(state)[getCol(c)];
+            if("in_number".equals(state)){
+                ans = ans * 10 + c - '0';
+                ans = sign ==1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+            }else if("signed".equals(state)){
+                sign = c == '+' ? 1 : -1;
+            }
+        }
+
+        /**
+         * 对于可能出现的字符情况，数字化
+         * @param c
+         * @return
+         */
+        private int getCol(char c){
+            //空格
+            if(c == ' '){
+                return 0;
+            }
+            if(c == '+' || c == '-'){
+                return 1;
+            }
+            //数字
+            if(Character.isDigit(c)){
+                return 2;
+            }
+            //其他的
+            return 3;
+        }
+
     }
 }
--- algo-jike/src/main/java/brushexercises/day59/NO_28_ImplementStrstr.java
+++ algo-jike/src/main/java/brushexercises/day59/NO_28_ImplementStrstr.java
@@ -5,8 +5,35 @@ package brushexercises.day59;
  */
 public class NO_28_ImplementStrstr {
 
-    public int strStr(String haystack, String needle) {
+    public static void main(String[] args) {
+        NO_28_ImplementStrstr implementStrstr = new NO_28_ImplementStrstr();
+        String haystack = "hello";
+        String needle = "ll";
+        int ans = implementStrstr.strStr(haystack, needle);
+        System.out.println(ans);
+    }
 
-        return 0;
+    /**
+     * 朴素解法：直接枚举所有原字符串
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        //注意:只需要n-m次外循环即可
+        for(int i=0;i<=n-m;i++){
+            int a = i;//haystack的指针，每次从i开发发起的
+            int b = 0;//needle的指针，每次从0开始发起
+            while (b < m && haystack.charAt(a) == needle.charAt(b)){
+                a++;
+                b++;
+            }
+            if(b == m){
+                return i;
+            }
+        }
+        return -1;
     }
 }
